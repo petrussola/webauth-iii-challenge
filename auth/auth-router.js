966659289b577808a51a9fc1d6a5abeb1e50fdc1
Pere <pere.solaclaver@gmail.com>
2019-11-13 14:24:41 +0000

    Generate token with jwt library

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,6 +3,7 @@
 const router = require("express").Router();
 const Users = require("../users/users-model");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", (req, res) => {
   const user = req.body;
@@ -24,7 +25,11 @@ router.post("/login", (req, res) => {
   Users.findUserBy({ username })
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.username}` });
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}`,
+          token: token
+        });
       } else {
         res.status(401).json({ message: `wrong credentials` });
       }
@@ -35,5 +40,19 @@ router.post("/login", (req, res) => {
         .json({ message: `Could not log you in: ${error.message}` });
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    department: user.department
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+  const result = jwt.sign(payload, process.env.JWT_SECRET, options);
+
+  return result;
+}
 
 module.exports = router;
--- users/users-router.js
+++ users/users-router.js
@@ -1,7 +1,8 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const restricted = require("./helpers/restricted-middleware");
 
 router.get("/", (req, res) => {
-    res.status(200).json({ message: `Hello from dummy endpoint` });
-  });
+  res.status(200).json({ message: `Hello from dummy endpoint` });
+});
 
 module.exports = router;
