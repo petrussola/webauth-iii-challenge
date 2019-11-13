f23489a2353d5f6b8b90527115e30a964bcf3c22
Pere <pere.solaclaver@gmail.com>
2019-11-13 13:00:15 +0000

    Migration to create users table

--- database/migrations/20191113125419_create-users-table.js
+++ database/migrations/20191113125419_create-users-table.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("users", table => {
+    table.increments();
+    table
+      .string("username", 128)
+      .notNullable()
+      .unique();
+    table.string("password", 128).notNullable();
+    table.string("department").notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("users");
 };
