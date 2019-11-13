350eb52d1c1db6cef7aaf3240408738523fc8689
Pere <pere.solaclaver@gmail.com>
2019-11-13 13:20:30 +0000

    Store password in DB with bcrypt

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,13 @@
+// IMPORT DEPENDENCIES
+
 const router = require("express").Router();
 const Users = require("../users/users-model");
+const bcrypt = require("bcryptjs");
 
 router.post("/register", (req, res) => {
   const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
   Users.insertUser(user)
     .then(data => {
       res.status(200).json(data);
